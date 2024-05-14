dcd26a5e48b9e399be411306c065f35b14555244
ayman <aemn22384@gmail.com>
2024-05-14 16:42:17 +0200

    add id to book data

--- data.js
+++ data.js
@@ -1,61 +1,71 @@
 const books = [{
+    id: 1,
     title: "The Great Gatsby",
     author: "F. Scott Fitzgerald",
-    price: "$10.99",
+    price: 10.9,
     url: "The_Great_Gatsby_Cover_1925_Retouched.jpg"
 },
 {
+    id: 2,
     title: "To Kill a Mockingbird",
     author: "Harper Lee",
     price: "$12.50",
     url: "To_kill_a_Mockingbird.jpg"
 },
 {
+    id: 3,
     title: "1984",
     author: "George Orwell",
-    price: "$9.99",
+    price: 9.99,
     url: "1984-Big-Brother.jpg"
 },
 {
+    id: 4,
     title: "The Catcher in the Rye",
     author: "J.D. Salinger",
-    price: "$11.25",
+    price: 11.2,
     url: "The_Catcher_in_the_Rye.jpg"
 },
 {
+    id: 5,
     title: "Animal Farm",
     author: "George Orwell",
-    price: "$8.99",
+    price: 8.99,
     url: "Animal_Farm.jpg"
 },
 {
+    id: 6,
     title: "Brave New World",
     author: "Aldous Huxley",
-    price: "$10.75",
+    price: 10.75,
     url: "BraveNewWorld.jpg"
 },
 {
+    id: 7,
     title: "The Grapes of Wrath",
     author: "John Steinbeck",
-    price: "$13.20",
+    price: 13.25,
     url: "The_Grapes_of_Wrath.jpg"
 },
 {
+    id: 8,
     title: "The Lord of the Rings",
     author: "J.R.R. Tolkien",
-    price: "$15.99",
+    price: 15.90,
     url: "the_lord-of-the-rings.png"
 },
 {
+    id: 9,
     title: "The Hobbit",
     author: "J.R.R. Tolkien",
-    price: "$9.50",
+    price: 9.50,
     url: "The_Hobbit.jpg"
 },
 {
+    id: 10,
     title: "Pride and Prejudice",
     author: "Jane Austen",
-    price: "$8.75",
+    price: 8.75,
     url: "PrideAndPrejudice.jpg"
 }
 ]
