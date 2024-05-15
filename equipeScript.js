a81fea394947544a72d5b646c5210fcc00a40360
ThibautG <thibaut.gaudinat@gmail.com>
2024-05-15 11:51:44 +0200

    bouton modifier

---
+++ equipeScript.js
@@ -0,0 +1,12 @@
+document.getElementById("edit-text-name").onclick = function() {
+    // Saisir les nouveaux détails
+    let nouveauNom = prompt("Entrez le nouveau nom :");
+    let nouveauPoste = prompt("Entrez le nouveau poste :");
+    let nouvelleDescription = prompt("Entrez la nouvelle description :");
+
+        // Modifiez les détails de la personne dans l'équipe
+    document.getElementById("tex-name").innerText = nouveauNom;
+    document.getElementById("tex-poste").innerText = nouveauPoste;
+    document.getElementById("tex-description").innerText = nouvelleDescription;
+    
+};
