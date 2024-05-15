3a38a0f2f5ba56176f2d3f362f46bd2b3ca63dcb
Rxxiv <r-git@vingt4.fr>
2024-05-15 09:18:16 +0200

    creation doss js/, modif index.html

---
+++ js/books.js
@@ -0,0 +1,82 @@
+const books = [{
+    id: 1,
+    title: "The Great Gatsby",
+    author: "F. Scott Fitzgerald",
+    price: 10.95,
+    url: "The_Great_Gatsby_Cover_1925_Retouched.jpg",
+	quantity: 0
+},
+{
+    id: 2,
+    title: "To Kill a Mockingbird",
+    author: "Harper Lee",
+    price: 12.55,
+    url: "To_kill_a_Mockingbird.jpg",
+	quantity: 0
+},
+{
+    id: 3,
+    title: "1984",
+    author: "George Orwell",
+    price: 9.99,
+    url: "1984-Big-Brother.jpg",
+	quantity: 0
+},
+{
+    id: 4,
+    title: "The Catcher in the Rye",
+    author: "J.D. Salinger",
+    price: 11.25,
+    url: "The_Catcher_in_the_Rye.jpg",
+	quantity: 0
+},
+{
+    id: 5,
+    title: "Animal Farm",
+    author: "George Orwell",
+    price: 8.99,
+    url: "Animal_Farm.jpg",
+	quantity: 0
+},
+{
+    id: 6,
+    title: "Brave New World",
+    author: "Aldous Huxley",
+    price: 10.75,
+    url: "BraveNewWorld.jpg",
+	quantity: 0
+},
+{
+    id: 7,
+    title: "The Grapes of Wrath",
+    author: "John Steinbeck",
+    price: 13.25,
+    url: "The_Grapes_of_Wrath.jpg",
+	quantity: 0
+},
+{
+    id: 8,
+    title: "The Lord of the Rings",
+    author: "J.R.R. Tolkien",
+    price: 15.95,
+    url: "the_lord-of-the-rings.png",
+	quantity: 0
+},
+{
+    id: 9,
+    title: "The Hobbit",
+    author: "J.R.R. Tolkien",
+    price: 9.55,
+    url: "The_Hobbit.jpg",
+	quantity: 0
+},
+{
+    id: 10,
+    title: "Pride and Prejudice",
+    author: "Jane Austen",
+    price: 8.75,
+    url: "PrideAndPrejudice.jpg",
+	quantity: 0
+}
+]
+
