d2d78219560cc3fd4d00a97ca22cbe1dff41e55d
ayman <aemn22384@gmail.com>
2024-05-15 17:49:50 +0200

    update js

--- js/panierScript.js
+++ js/panierScript.js
@@ -1,13 +1,29 @@
 let booksId = [0, 1, 4, 6]
+let count = 0
+putInPanier(booksId, count)
+function handlePlusFn() {
+    let count = document.getElementById(`C-${this.id}`) // get the count element
+    let price = document.querySelector("li")
 
-putInPanier(booksId)
+    if (+count.textContent < 10) {
+        count.textContent++
+    }
+}
+function handleMoinsFn() {
+    let count = document.getElementById(`C-${this.id}`) // get the count element
 
-function putInPanier(booksId) {
+    if (+count.textContent > 0) {
+        count.textContent--
+
+    }
+}
+
+function putInPanier(booksId, bookCount) {
     booksId.map((id) => books[id]).forEach((bookObj) => {
         // create element html
         let myMainElement = document.querySelector(".books")
+        let info = document.createElement("div")
         let book = document.createElement("li")
-        let btns = document.createElement("div")
 
 
 
@@ -25,11 +41,22 @@ function putInPanier(booksId) {
 
         let title = document.createElement("h3");
 
-        title.append(bookObj.title); // Add book title
+        title.append(bookObj.title);    // Add book title
 
-        book.appendChild(title); // Add title To book    
+        title.className = "title"       // Add class to the title
+
+        info.appendChild(title);        // Add title To info    
 
         // end book title
+
+        // start book author
+        let author = document.createElement("div")
+
+        author.append(bookObj.author) //add author name
+
+        info.appendChild(author) // Add author to info
+
+        // end book author
 
         // start book price
 
@@ -37,27 +64,27 @@ function putInPanier(booksId) {
 
         price.append(`$${bookObj.price}`) // Add price Text
 
-        book.appendChild(price); // Add price To book
+        price.className = "price"         // Ass class to the price
+
+        info.appendChild(price); // Add price To info
 
         // end book price
 
-        // start book author
-        let author = document.createElement("div")
-
-        author.append(bookObj.author) //add author name
-
-        book.appendChild(author) // Add author to book
-
-        // end book author
 
         // start book btn
-        let plusIcon = document.createElement("i")
+        let btns = document.createElement("div")
+
         let plusBtn = document.createElement("button")
-        let moinsIcon = document.createElement("i")
         let moinsBtn = document.createElement("button")
+
+        let plusIcon = document.createElement("i")
+        let moinsIcon = document.createElement("i")
+
         let count = document.createElement("span")
 
-        count.append(bookObj.price)
+
+        count.append(bookCount)
+        count.id = `C-${bookObj.id}`
 
         plusIcon.className = "fas fa-circle-plus"  // add icon class 
         moinsIcon.className = "fas fa-circle-minus" // add icon class 
@@ -65,17 +92,25 @@ function putInPanier(booksId) {
         plusBtn.append(plusIcon)  // add plusBtn icon 
         moinsBtn.append(moinsIcon) // add moinsBtn icon 
 
-        plusBtn.id = bookObj.id // add book id to btns
-        moinsBtn.id = bookObj.id // add book id to btns
+        plusBtn.className = "plus-btn" // add class  to btns
+        moinsBtn.className = "moins-btn"// add class to btns
+
+        plusBtn.id = bookObj.id // add book-id to btns
+        moinsBtn.id = bookObj.id// add book-id to btns
+
+        plusBtn.onclick = handlePlusFn
+        moinsBtn.onclick = handleMoinsFn
 
         btns.appendChild(moinsBtn) // add moinsBtn to btns
         btns.appendChild(count)
         btns.appendChild(plusBtn)  // add plusBtn to btns
 
-        book.append(btns)  // add btns to book
+        info.append(btns)  // add btns to info
 
         // end book btn
 
+        // Add info to book
+        book.appendChild(info)
         // Add id to book bookObj
         book.id = bookObj.id;
 
