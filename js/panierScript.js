f6e83a81804e24d9e7bf75ef4ba0732b3089b53e
ayman <aemn22384@gmail.com>
2024-05-15 13:50:27 +0200

    update script

--- js/panierScript.js
+++ js/panierScript.js
@@ -1,64 +1,77 @@
 books.forEach((bookObj) => {
+    // create element html
     let myMainElement = document.querySelector(".books")
     let book = document.createElement("li")
-    let img = document.createElement("img")
-    let title = document.createElement("h3");
-    let author = document.createElement("div")
-    let price = document.createElement("div");
     let btns = document.createElement("div")
+
+    let count = document.createElement("span")
+
+
+    // start Book images
+
+    let img = document.createElement("img")
+
+    img.src = `../images/books-images/${bookObj.url}` // Add the link of the img
+
+    book.append(img) // Add img to book
+
+    // end Book images
+
+    // start book title
+
+    let title = document.createElement("h3");
+
+    title.append(bookObj.title); // Add book title
+
+    book.appendChild(title); // Add title To book    
+
+    // end book title
+
+    // start book price
+
+    let price = document.createElement("div");
+
+    price.append(`$${bookObj.price}`) // Add price Text
+
+    book.appendChild(price); // Add price To book
+
+    // end book price
+
+    // start book author
+    let author = document.createElement("div")
+
+    author.append(bookObj.author) //add author name
+
+    book.appendChild(author) // Add author to book
+
+    // end book author
+
+    // start book btn
     let plusIcon = document.createElement("i")
     let plusBtn = document.createElement("button")
     let moinsIcon = document.createElement("i")
     let moinsBtn = document.createElement("button")
 
+    plusIcon.className = "fas fa-circle-plus"  // add icon class 
+    moinsIcon.className = "fas fa-circle-minus" // add icon class 
 
-    // Add the link of the img
-    img.src = `../images/books-images/${bookObj.url}`
+    plusBtn.append(plusIcon)  // add plusBtn icon 
+    moinsBtn.append(moinsIcon) // add moinsBtn icon 
 
-    // Add book title
-    title.append(bookObj.title);
+    plusBtn.id = bookObj.id // add book id to btns
+    moinsBtn.id = bookObj.id // add book id to btns
 
-    //add author name
-    author.append(bookObj.author)
+    btns.appendChild(moinsBtn) // add moinsBtn to btns
+    btns.appendChild(plusBtn)  // add plusBtn to btns
 
-    // Add price Text
-    price.append(`$${bookObj.price}`);
+    book.append(btns)  // add btns to book
 
-    // add icon class 
-    plusIcon.className = "fas fa-circle-plus"
-
-    // add icon class 
-    moinsIcon.className = "fas fa-circle-minus"
-
-    // add plusBtn icon 
-    plusBtn.append(plusIcon)
-
-    // add moinsBtn icon 
-    moinsBtn.append(moinsIcon)
-
-    // Add img to book
-    book.append(img)
-
-    // Add title To book
-    book.appendChild(title);
-
-    // Add price To book
-    book.appendChild(price);
-
-    // Add author to book
-    book.appendChild(author)
-
-    // add moinsBtn to btns
-    btns.appendChild(moinsBtn)
-
-    // add plusBtn to btns
-    btns.appendChild(plusBtn)
-
-    // add btns to book
-    book.append(btns)
+    // end book btn
 
     // Add id to book bookObj
     book.id = bookObj.id;
 
+    // add book to html ul
     myMainElement.appendChild(book)
 })
+
