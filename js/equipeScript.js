1503cf05a0e74bd9371451187dd75b114c354eb7
ThibautG <thibaut.gaudinat@gmail.com>
2024-05-16 12:07:56 +0200

    ajout des photo dans les modifs

--- js/equipeScript.js
+++ js/equipeScript.js
@@ -1,20 +1,28 @@
 //MODIFIER UN MEMBRE
-function editEquipier (btn, name, poste, description){
-    let bouton = document.getElementById(btn);
+function editEquipier (btn, name, poste, description, photo){
+    const bouton = document.getElementById(btn);
     bouton.addEventListener('click', function() {
         // Saisir les nouveaux détails
-        let nouveauNom = prompt("Entrez le nouveau nom :");
-        let nouveauPoste = prompt("Entrez le nouveau poste :");
-        let nouvelleDescription = prompt("Entrez la nouvelle description :");
+        const nouvelleImage = prompt("Entrez l'url de la nouvelle image :")
+        const nouveauNom = prompt("Entrez le nouveau nom :");
+        const nouveauPoste = prompt("Entrez le nouveau poste :");
+        const nouvelleDescription = prompt("Entrez la nouvelle description :");
+
+        //Récupérer les éléments du HTML    
+        const ancienImg = document.getElementById(photo)
+        const ancienNom = document.getElementById(name);
+        const ancienPoste = document.getElementById(poste);
+        const ancienneDescription = document.getElementById(description);
 
         // Vérifier si l'utilisateur a entré des détails valides
         if (nouveauNom !== null && nouveauNom !== "" &&
-            nouveauPoste !== null && nouveauPoste !== "" &&
-            nouvelleDescription !== null && nouvelleDescription !== "") {
+        nouveauPoste !== null && nouveauPoste !== "" &&
+        nouvelleDescription !== null && nouvelleDescription !== "") {
             // Modifier les détails de la personne dans l'équipe
-            document.getElementById(name).innerText = nouveauNom;
-            document.getElementById(poste).innerText = nouveauPoste;
-            document.getElementById(description).innerText = nouvelleDescription;
+            ancienNom.textContent = nouveauNom;
+            ancienPoste.textContent = nouveauPoste;
+            ancienneDescription.textContent = nouvelleDescription;
+            ancienImg.setAttribute("src",nouvelleImage)
         } else {
             // Afficher un message si des détails valides n'ont pas été entrés
             alert("Veuillez entrer des détails valides pour la personne.");
@@ -22,10 +30,10 @@ function editEquipier (btn, name, poste, description){
     });
 }
 
-editEquipier("paige-btn-edit", "paige-name", "paige-poste", "paige-description");
-editEquipier("lex-btn-edit", "lex-name", "lex-poste", "lex-description");
-editEquipier("belle-btn-edit", "belle-name", "belle-poste", "belle-description");
-// editEquipier("ash-btn-edit", "ash-name", "ash-poste", "ash-description");
+editEquipier("paige-btn-edit", "paige-name", "paige-poste", "paige-description", "paige-photo");
+editEquipier("lex-btn-edit", "lex-name", "lex-poste", "lex-description", "lex-photo");
+editEquipier("belle-btn-edit", "belle-name", "belle-poste", "belle-description", "belle-photo");
+
 
 
 //AJOUTER UN MEMBRE
@@ -41,17 +49,18 @@ form.addEventListener('submit', function(event) {
     const fullName = document.getElementById('full-name').value;
     const poste = document.getElementById('poste').value;
     const description = document.getElementById('description').value;
+    const photo = document.getElementById('photo-url').value
 
     // Création d'un nouveau bloc HTML avec les données du nouveau membre
     const newMemberHTML = `
         <div class="equipier">
             <div class="equipier-photo">
-            <img src="images/equipe-images/owl.jpg" alt="owl-photo" id="owl-photo" />
+            <img src="${photo}" alt="new-photo" id="new-photo" />
             </div>
-            <h2>${fullName}</h2>
-            <h3>${poste}</h3>
-            <p>${description}</p>
-            <button>Modifier</button>
+            <h2 id="new-name">${fullName}</h2>
+            <h3 id="new-poste">${poste}</h3>
+            <p id="paige-description">${description}</p>
+            <button id="new-button-edit">Modifier</button>
         </div>
     `;
 
@@ -60,6 +69,8 @@ form.addEventListener('submit', function(event) {
 
     // Insertion du nouveau bloc dans la section de l'équipe
     equipeContenu.innerHTML += newMemberHTML;
+    //Rendre le nouveau membre modifiable
+    editEquipier("new-button-edit", "new-name", "new-poste", "new-description", "new-photo");
 
   
 
